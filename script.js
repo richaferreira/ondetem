fdd0e031c0e113ea4b871d4b0bc89487a5fe574b
Richardson Ferreira <richardsonferreira1995@gmail.com>
2026-04-13 20:42:02 -0300

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@
 // ============================================
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/service-worker.js')
+        navigator.serviceWorker.register('./service-worker.js')
             .then(reg => console.log('✓ Service Worker registrado:', reg.scope))
             .catch(err => console.log('✗ Erro ao registrar SW:', err));
     });
